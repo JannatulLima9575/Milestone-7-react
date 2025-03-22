a544ec0a521219d6414327fcf987ddc2b5bf85c6
Jannatul <limajannat1675@gmail.com>
2025-03-23 04:27:43 +0600

    added react basic concept

---
+++ module-38/explore-core-react/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Core Concept</h1>
+    </>
+  )
+}
+
+function Lima() {
+  const country = 'Bangladesh';
+  return (
+    <>
+      <h1>My Self</h1>
+      <p>I'm from {country}</p>
+    </>
+  );
+}
+export default App
