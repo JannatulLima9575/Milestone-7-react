e84203e3e6b54288fc6953254f289a1c253d25d0
Jannatul <limajannat1675@gmail.com>
2025-03-23 01:14:32 +0600

    added js fundamentals

---
+++ module-37-(recap-JS)/recap.js
@@ -0,0 +1,20 @@
+// # these JavaScript Fundamentals that you need to know
+/* 
+1. use let and const instead of var
+2. clear about the loop(if, else, if else)
+3. array: declare, length, index, push, pop, indexof, includes
+4. object: declare, set value, get value, keys, values
+5. function: declare, call, parameter and return value
+6. destructuring: object destructuring array destructuring
+7. template string, arrow function, spread operator
+8. map, filter, find, forEach
+9. class and constructor
+10. inheritance, extends class, supper, class method
+11. module system, import, export
+12. async await, fetch, promise
+13. local storage, session storage
+14. JSON, JSON.pase, JSON.stringify
+15. error handling: try, catch, finally
+16. regular expression
+17. this keyword 
+*/
